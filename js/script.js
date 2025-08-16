6342e133ac10c6b3ce73b9dcdad938f01b3a29d6
egaadyatma <egaadyatmapuspito@gmail.com>
2025-08-16 15:48:31 +0700

    update responsive

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+const wrapper = document.getElementById("wrapper");
+
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("hilang");
+    wrapper.classList.toggle("borderhiden");
+
+    const icon = menuIcon.querySelector("i");
+    if (icon.classList.contains("ph-list")) {
+        icon.classList.remove("ph-list");
+        icon.classList.add("ph-x");   // ikon close
+    } else {
+        icon.classList.remove("ph-x");
+        icon.classList.add("ph-list"); // balik ke hamburger
+    }
+});
