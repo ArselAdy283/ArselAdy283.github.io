b2bf246db738081c65b557f78a0d6775cb90cf87
egaadyatma <egaadyatmapuspito@gmail.com>
2025-08-27 21:30:35 +0700

    adono

--- js/typed.js
+++ js/typed.js
@@ -1,5 +1,5 @@
 let typed = new Typed("#typed", {
-        strings: ["Orang biasa", "Anak RPL", "Bocah SMEA", "Murid king fathur", "Player Menkrep", "Orang JAWA"],
+        strings: ["Orang biasa", "Anak RPL", "Bocah SMEA", "Player Menkrep", "Orang JAWA"],
         typeSpeed: 60,
         backSpeed: 30,
         loop: true
